2fc224979582f62abdfa43e2f779ae53b31275c5
Traxmandl <ai01testy@gmail.com>
2026-03-09 19:24:01 +0100

    Zlepši jazykové přepínání

--- .next.bak.1772797905/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/jindrichtraxmandl/Projects/Mlýn na Pile mlynnapile.cz/app/layout\":[\"static/media/13971731025ec697-s.p.woff2\",\"static/media/1f3fe8c6df3d47c1-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
